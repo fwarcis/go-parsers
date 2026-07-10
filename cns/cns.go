6fced86330824c27e8be4650bd3f82322ab22389
fwarcis <fwarcis@gmail.com>
2026-07-10 00:12:49 +0000

    init

---
+++ cns/cns.go
@@ -0,0 +1,89 @@
+package cns
+
+type Peeker[T any] interface {
+	Advance()
+	Peek() (T, bool)
+	Err() error
+}
+
+type Adder[T any] interface {
+	Add(T) error
+}
+
+type Consumer[V any] struct {
+	p Peeker[V]
+	a Adder[V]
+}
+
+func New[V any](p Peeker[V], a Adder[V]) Consumer[V] {
+	return Consumer[V]{p, a}
+}
+
+func (c *Consumer[V]) Exactly(n int, can func(V) bool) (bool, error) {
+	for range n {
+		value, has := c.p.Peek()
+		if !has {
+			return false, c.p.Err()
+		}
+		if !can(value) {
+			return false, nil
+		}
+		if err := c.a.Add(value); err != nil {
+			return false, err
+		}
+		c.p.Advance()
+	}
+	if v, has := c.p.Peek(); has && can(v) {
+		return false, nil
+	}
+	return true, c.p.Err()
+}
+
+func (c *Consumer[V]) Minimum(n int, can func(V) bool) (bool, error) {
+	for range n {
+		value, has := c.p.Peek()
+		if !has {
+			return false, c.p.Err()
+		}
+		if !can(value) {
+			return false, nil
+		}
+		if err := c.a.Add(value); err != nil {
+			return false, err
+		}
+		c.p.Advance()
+	}
+	for {
+		value, has := c.p.Peek()
+		if !has {
+			return true, c.p.Err()
+		}
+		if !can(value) {
+			return true, nil
+		}
+		if err := c.a.Add(value); err != nil {
+			return true, err
+		}
+		c.p.Advance()
+	}
+}
+
+func (c *Consumer[V]) Maximum(n int, can func(V) bool) (bool, error) {
+	for range n {
+		value, has := c.p.Peek()
+		if !has {
+			return true, c.p.Err()
+		}
+		if !can(value) {
+			return true, nil
+		}
+		if err := c.a.Add(value); err != nil {
+			return true, err
+		}
+		c.p.Advance()
+	}
+	if v, ok := c.p.Peek(); ok && can(v) {
+		return false, nil
+	}
+	return true, c.p.Err()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fwarcis/go-parsers
+
+go 1.26.5
