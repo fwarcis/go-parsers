69b9207b43a8f9f7ca63d708c144f78b779666b9
fwarcis <fwarcis@gmail.com>
2026-08-01 00:07:12 +0500

    feat(cns): Return 'Result' struct with 'Ok' and 'Err' in methods of 'Consumer'

--- cns/cns.go
+++ cns/cns.go
@@ -23,90 +23,95 @@ func New[V any](p Peeker[V], a Adder[V]) Consumer[V] {
 	return Consumer[V]{p, a}
 }
 
-func (c *Consumer[V]) Exactly(n int, can func(V) bool) (bool, error) {
+type Result struct {
+	Ok  bool
+	Err error
+}
+
+func (c *Consumer[V]) Exactly(n int, can func(V) bool) Result {
 	for range n {
 		value, has := c.p.Peek()
 		if !has {
-			return false, c.p.Err()
+			return Result{false, c.p.Err()}
 		}
 		if !can(value) {
-			return false, nil
+			return Result{false, nil}
 		}
 		if err := c.a.Add(value); err != nil {
-			return false, err
+			return Result{false, err}
 		}
 		c.p.Advance()
 	}
 	if v, has := c.p.Peek(); has && can(v) {
-		return false, nil
+		return Result{false, nil}
 	}
-	return true, c.p.Err()
+	return Result{true, c.p.Err()}
 }
 
-func (c *Consumer[V]) Minimum(n int, can func(V) bool) (bool, error) {
+func (c *Consumer[V]) Minimum(n int, can func(V) bool) Result {
 	for range n {
 		value, has := c.p.Peek()
 		if !has {
-			return false, c.p.Err()
+			return Result{false, c.p.Err()}
 		}
 		if !can(value) {
-			return false, nil
+			return Result{false, nil}
 		}
 		if err := c.a.Add(value); err != nil {
-			return false, err
+			return Result{false, err}
 		}
 		c.p.Advance()
 	}
 	for {
 		value, has := c.p.Peek()
 		if !has {
-			return true, c.p.Err()
+			return Result{true, c.p.Err()}
 		}
 		if !can(value) {
-			return true, nil
+			return Result{true, nil}
 		}
 		if err := c.a.Add(value); err != nil {
-			return true, err
+			return Result{true, err}
 		}
 		c.p.Advance()
 	}
 }
 
-func (c *Consumer[V]) Maximum(n int, can func(V) bool) (bool, error) {
+func (c *Consumer[V]) Maximum(n int, can func(V) bool) Result {
 	for range n {
 		value, has := c.p.Peek()
 		if !has {
-			return true, c.p.Err()
+			return Result{true, c.p.Err()}
 		}
 		if !can(value) {
-			return true, nil
+			return Result{true, nil}
 		}
 		if err := c.a.Add(value); err != nil {
-			return true, err
+			return Result{true, err}
 		}
 		c.p.Advance()
 	}
 	if v, ok := c.p.Peek(); ok && can(v) {
-		return false, nil
+		return Result{false, nil}
 	}
-	return true, c.p.Err()
+	return Result{true, c.p.Err()}
 }
 
-func (c *Consumer[V]) ForEach(n int, sequence iter.Seq[V]) (bool, error) {
+func (c *Consumer[V]) ForEach(n int, sequence iter.Seq[V]) Result {
 	for range n {
 		for elem := range sequence {
 			value, has := c.p.Peek()
 			if !has {
-				return false, c.p.Err()
+				return Result{false, c.p.Err()}
 			}
 			if any(elem) != any(value) {
-				return false, nil
+				return Result{false, nil}
 			}
 			if err := c.a.Add(value); err != nil {
-				return false, err
+				return Result{false, err}
 			}
 			c.p.Advance()
 		}
 	}
-	return true, c.p.Err()
+	return Result{true, c.p.Err()}
 }
