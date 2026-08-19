4010e4df145b156a23c224d534e827231a74d4c9
fwarcis <fwarcis@gmail.com>
2026-08-20 03:01:52 +0500

    refact(cns): 'cns' to 'csmr'

---
+++ csmr/adders.go
@@ -0,0 +1,18 @@
+package csmr
+
+type RuneWriter interface {
+	WriteRune(rn rune) (n int, err error)
+}
+
+type RuneAdder struct {
+	w RuneWriter
+}
+
+func NewRuneAdder(w RuneWriter) *RuneAdder {
+	return &RuneAdder{w}
+}
+
+func (a *RuneAdder) Add(r rune) error {
+	_, err := a.w.WriteRune(r)
+	return err
+}
---
+++ csmr/peekers.go
@@ -0,0 +1,40 @@
+package csmr
+
+import (
+	"io"
+)
+
+type _runeBuffer struct {
+	rune   rune
+	isRead bool
+}
+
+type RunePeeker struct {
+	r io.RuneReader
+
+	buf _runeBuffer
+	err error
+}
+
+func NewRunePeeker(r io.RuneReader) *RunePeeker {
+	p := &RunePeeker{r: r}
+	p.Advance()
+	return p
+}
+
+func (p *RunePeeker) Advance() {
+	r, _, err := p.r.ReadRune()
+	p.err = err
+	p.buf = _runeBuffer{
+		rune:   r,
+		isRead: err == nil,
+	}
+}
+
+func (p *RunePeeker) Peek() (rune, bool) {
+	return p.buf.rune, p.buf.isRead
+}
+
+func (p *RunePeeker) Err() error {
+	return p.err
+}
