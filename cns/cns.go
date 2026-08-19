351dae8b6f18073f2eda08d96dbb1c1d72d2d02a
fwarcis <fwarcis@gmail.com>
2026-08-20 02:38:01 +0500

    refact(cns): More clear field names for 'Peeker' and 'Adder'

--- cns/cns.go
+++ cns/cns.go
@@ -15,15 +15,18 @@ type Adder[V any] interface {
 }
 
 type Consumer[V any] struct {
-	p Peeker[V]
-	a Adder[V]
+	pkr Peeker[V]
+	adr Adder[V]
 
 	err      error
 	isFailed bool
 }
 
-func New[V any](p Peeker[V], a Adder[V]) Consumer[V] {
-	return Consumer[V]{p, a, nil, false}
+func New[V any](pkr Peeker[V], adr Adder[V]) Consumer[V] {
+	return Consumer[V]{
+		pkr: pkr,
+		adr: adr,
+	}
 }
 
 type Mode uint64
@@ -46,23 +49,23 @@ func (c *Consumer[V]) Exactly(
 		defer c.failIfNot(&ok)
 	}
 	for range n {
-		val, has := c.p.Peek()
+		val, has := c.pkr.Peek()
 		if !has {
-			c.err = c.p.Err()
+			c.err = c.pkr.Err()
 			return false
 		}
 		if !can(val) {
 			return false
 		}
-		c.err = c.a.Add(val)
+		c.err = c.adr.Add(val)
 		if c.err != nil {
 			return false
 		}
-		c.p.Advance()
+		c.pkr.Advance()
 	}
-	val, has := c.p.Peek()
+	val, has := c.pkr.Peek()
 	if !has {
-		c.err = c.p.Err()
+		c.err = c.pkr.Err()
 		return true
 	}
 	return !can(val)
@@ -83,34 +86,34 @@ func (c *Consumer[V]) Minimum(
 		return false
 	}
 	for range n {
-		val, has := c.p.Peek()
+		val, has := c.pkr.Peek()
 		if !has {
-			c.err = c.p.Err()
+			c.err = c.pkr.Err()
 			return false
 		}
 		if !can(val) {
 			return false
 		}
-		c.err = c.a.Add(val)
+		c.err = c.adr.Add(val)
 		if c.err != nil {
 			return false
 		}
-		c.p.Advance()
+		c.pkr.Advance()
 	}
 	for {
-		value, has := c.p.Peek()
+		value, has := c.pkr.Peek()
 		if !has {
-			c.err = c.p.Err()
+			c.err = c.pkr.Err()
 			return true
 		}
 		if !can(value) {
 			return true
 		}
-		c.err = c.a.Add(value)
+		c.err = c.adr.Add(value)
 		if c.err != nil {
 			return true
 		}
-		c.p.Advance()
+		c.pkr.Advance()
 	}
 }
 
@@ -129,23 +132,23 @@ func (c *Consumer[V]) Maximum(
 		return true
 	}
 	for range n {
-		val, has := c.p.Peek()
+		val, has := c.pkr.Peek()
 		if !has {
-			c.err = c.p.Err()
+			c.err = c.pkr.Err()
 			return true
 		}
 		if !can(val) {
 			return true
 		}
-		c.err = c.a.Add(val)
+		c.err = c.adr.Add(val)
 		if c.err != nil {
 			return true
 		}
-		c.p.Advance()
+		c.pkr.Advance()
 	}
-	val, has := c.p.Peek()
+	val, has := c.pkr.Peek()
 	if !has {
-		c.err = c.p.Err()
+		c.err = c.pkr.Err()
 		return true
 	}
 	return !can(val)
@@ -170,19 +173,19 @@ func (c *Consumer[V]) ForEach(
 	}
 	for range n {
 		for elem := range sequence {
-			val, has := c.p.Peek()
+			val, has := c.pkr.Peek()
 			if !has {
-				c.err = c.p.Err()
+				c.err = c.pkr.Err()
 				return false
 			}
 			if any(elem) != any(val) {
 				return false
 			}
-			c.err = c.a.Add(val)
+			c.err = c.adr.Add(val)
 			if c.err != nil {
 				return false
 			}
-			c.p.Advance()
+			c.pkr.Advance()
 		}
 	}
 	return true
